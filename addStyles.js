7160bc37fb42da3804de4c00f8aed678239af76b
Franco D'Agostino <144281050+Francodago@users.noreply.github.com>
2025-05-08 14:55:06 -0400

    Initial calculator complete – will refine with more Tailwind as I learn

--- addStyles.js
+++ addStyles.js
@@ -1,8 +1,8 @@
-const bodyClass="bg-purple-800 bg-gradient-to-br from-blue-300 bg-opacity-25";
+const bodyClass="bg-green-800 bg-gradient-to-br from-blue-300 bg-opacity-25";
 const bdy=document.querySelector("body");
 bodyClass.split(" ").forEach((cls) =>bdy.classList.add(cls));
 
-const calcBodyClass ="max-lg rounded overflow-hidden shadow-2xl bg-pink-700 bg-opacity-75 bg-gradient-to-tr from-gray-800 flex";
+const calcBodyClass ="max-lg rounded overflow-hidden shadow-2xl bg-yellow-300 bg-opacity-75 bg-gradient-to-tr from-purple-800 flex";
 const caclbdy=document.querySelector(".calcBody");
 calcBodyClass.split(" ").forEach((cls) =>caclbdy.classList.add(cls));
 
